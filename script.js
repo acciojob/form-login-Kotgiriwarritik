5236d6f26c7f76fd09c10ea4899549b0e816e50f
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2023-11-28 15:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+var x=document.getElementById("form1");
+let name = ""
+for (var i=0;i<x.length;i++) {
+if (x.elements[i].value!='Submit')
+name += x.elements[i].value + " "
+}
 }
